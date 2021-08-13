42ecdf52af506d881b84f27cc95e72878b91685a
muskankumarisingh <81345123+muskankumarisingh@users.noreply.github.com>
2021-08-13 14:44:16 +0530

    Add files via upload

---
+++ my_cow_bull.py
@@ -0,0 +1,56 @@
+import random
+digit=5
+name=input("enter your name :-")
+print("Hello",name,"WELCOME TO COW AND BULL GAME\U0001F917")
+print("*****************************************")
+def getsecretNum():
+    number=list(range(10))
+    random.shuffle(number)
+    return number
+def getclues():
+    numbers=getsecretNum()
+    secret_num=[]
+    for i in range(digit):
+        secret_num.append(numbers[i])
+    return secret_num
+def check_guess():
+    bull=[]
+    cow=[]
+    num=getclues()
+    i=0
+    print(num)
+    maxguess=10
+    while maxguess>0:
+        guess=int(input("enter your guess="))
+        position=int(input("enter the position of your number="))
+        print("-----------------------")
+        if  guess in num:
+            index = num.index(guess)
+            if index== position: 
+                if guess not in bull:
+                    bull.insert(index,guess)
+                    print("Bull",bull)
+                else:
+                    print("You Already Used This digit Try  any Different digit")
+            else:
+                cow.append(guess)
+                print("Cow,the position of number is not same ,you can resuse this number :")
+        if bull==num:
+            print(name,"Congratulations you win the game\U0001F917")
+            break 
+        maxguess-=1
+        print(maxguess,"Turns are left")
+    else:
+        print("You ran out your tries, You Loose The Game")   
+    return bull
+check_guess() 
+def play_again():
+    while True:
+        again=input("If you Want to play again press y for yes or N of No =") 
+        if again=="y":
+            check_guess()
+        else:
+            break 
+play_again()
+
+
